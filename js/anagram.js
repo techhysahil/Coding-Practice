fa09d7bfafb995b6074e211d949c38665388af53
Sahil <sahilkkrazy@gmail.com>
2016-09-30 19:47:05 +0530

    added anagram string code

---
+++ js/anagram.js
@@ -0,0 +1,10 @@
+var first_str = document.getElementById('first_string').value,
+	second_str = document.getElementById('second_string').value;
+
+	if(first_str && second_str){
+
+	}else{
+		if(first_str === ""&& second_str === ""){
+			alert("Fields cann't be empty");
+		}
+	}
